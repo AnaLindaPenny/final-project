a06578d8be93048d117f241897a4b1c36f692997
Kelsey Vermeer <kelseyvrmr@yahoo.com>
2021-08-09 19:30:29 -0400

    update heroku

---
+++ Resources/schema.sql
@@ -0,0 +1,13 @@
+-- DROP Table combinedboxoffice;
+
+CREATE Table combinedboxoffice(
+	universe_code INT,	
+	universe_name VARCHAR (10),	
+	movie_code INT,	
+	movie_name VARCHAR (75),	
+	country_code INT,
+	country_name VARCHAR (75),
+	release_date DATE,
+	opening	INT,
+	gross INT)
+	
