2f60728505a5d62fed4deb937e70be23d06efce6
Zavala <toro.d.c@gmail.com>
2021-08-11 14:08:30 -0400

    revenue scatter chart and bubble chart edits

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -5,117 +5,132 @@ d3.json("../Resources/final_MvDC.json").then(json => {
 
     // CHART 1
     // structure data
-    var mcu_values = []
-    var dceu_values = []
-    var xmen_values = []
-    var supe_values = []
-    var batman_values = []
+    gross_revs = []
+
+    json_data.forEach(item => {
+        var point = {
+            x: item.year,
+            y: item.imdb_gross
+        }
+        gross_revs.push(point)
+    })
+
+    console.log(gross_revs)
+    // var mcu_values = []
+    // var dceu_values = []
+    // var xmen_values = []
+    // var supe_values = []
+    // var batman_values = []
     
-    var mcu_films = json_data.filter(film => film.universe_code === 1000)
-    console.log(mcu_films)
+    // var mcu_films = json_data.filter(film => film.universe_code === 1000)
+    // console.log(mcu_films)
 
-    var dceu_films = json_data.filter(film => film.universe_code === 2000)
-    console.log(dceu_films)
+    // var dceu_films = json_data.filter(film => film.universe_code === 2000)
+    // console.log(dceu_films)
 
-    var xmen_films = json_data.filter(film => film.universe_code === 3000)
-    console.log(xmen_films)
+    // var xmen_films = json_data.filter(film => film.universe_code === 3000)
+    // console.log(xmen_films)
 
-    var supe_films = json_data.filter(film => film.universe_code === 4000)
-    console.log(supe_films)
+    // var supe_films = json_data.filter(film => film.universe_code === 4000)
+    // console.log(supe_films)
 
-    var batman_films = json_data.filter(film => film.universe_code === 5000)
-    console.log(batman_films)
+    // var batman_films = json_data.filter(film => film.universe_code === 5000)
+    // console.log(batman_films)
 
-    mcu_films.forEach(item => {
-        mcu_values.push(item)        
-    })
+    // mcu_films.forEach(item => {
+    //     mcu_values.push(item)        
+    // })
 
-    dceu_films.forEach(item => {
-        dceu_values.push(item)        
-    })
+    // dceu_films.forEach(item => {
+    //     dceu_values.push(item)        
+    // })
 
-    xmen_films.forEach(item => {
-        xmen_values.push(item)        
-    })
+    // xmen_films.forEach(item => {
+    //     xmen_values.push(item)        
+    // })
 
-    supe_films.forEach(item => {
-        supe_values.push(item)        
-    })
+    // supe_films.forEach(item => {
+    //     supe_values.push(item)        
+    // })
 
-    batman_films.forEach(item => {
-        batman_values.push(item)        
-    })
+    // batman_films.forEach(item => {
+    //     batman_values.push(item)        
+    // })
 
-    var mcu_points = []
-    var dceu_points = []
-    var xmen_points = []
-    var batman_points = []
-    var supe_points = []
+    // var mcu_points = []
+    // var dceu_points = []
+    // var xmen_points = []
+    // var batman_points = []
+    // var supe_points = []
 
-    mcu_values.forEach(item => {
-        var point = {
-            x: item.year,
-            y: item.imdb_gross
-        }
-        mcu_points.push(point)
-    })
+    // mcu_values.forEach(item => {
+    //     var point = {
+    //         x: item.year,
+    //         y: item.imdb_gross
+    //     }
+    //     mcu_points.push(point)
+    // })
 
-    dceu_values.forEach(item => {
-        var point = {
-            x: item.year,
-            y: item.imdb_gross
-        }
-        dceu_points.push(point)
-    })
+    // dceu_values.forEach(item => {
+    //     var point = {
+    //         x: item.year,
+    //         y: item.imdb_gross
+    //     }
+    //     dceu_points.push(point)
+    // })
 
-    xmen_values.forEach(item => {
-        var point = {
-            x: item.year,
-            y: item.imdb_gross
-        }
-        xmen_points.push(point)
-    })
+    // xmen_values.forEach(item => {
+    //     var point = {
+    //         x: item.year,
+    //         y: item.imdb_gross
+    //     }
+    //     xmen_points.push(point)
+    // })
 
-    batman_values.forEach(item => {
-        var point = {
-            x: item.year,
-            y: item.imdb_gross
-        }
-        batman_points.push(point)
-    })
+    // batman_values.forEach(item => {
+    //     var point = {
+    //         x: item.year,
+    //         y: item.imdb_gross
+    //     }
+    //     batman_points.push(point)
+    // })
 
-    supe_values.forEach(item => {
-        var point = {
-            x: item.year,
-            y: item.imdb_gross
-        }
-        supe_points.push(point)
-    })
+    // supe_values.forEach(item => {
+    //     var point = {
+    //         x: item.year,
+    //         y: item.imdb_gross
+    //     }
+    //     supe_points.push(point)
+    // })
     
     //  chart setup
     const data1 = {
-        datasets: [
-            {label: 'MCU',
-            data: mcu_points,
-            backgroundColor: 'rgb(255,0,0)'
-            },
-            {label: 'DCEU',
-            data: dceu_points,
-            backgroundColor: 'rgb(0,0,255)'
-            },
-            {label: 'X-Men',
-            data: xmen_points,
-            backgroundColor: 'rgb(255,255,0)'
-            },
-            {label: 'Batman',
-            data: batman_points,
+        datasets: [{
+            lable: "Films",
+            data: gross_revs,
             backgroundColor: 'rgb(0,0,0)'
-            },
-            {label: 'Superman',
-            data: supe_points,
-            backgroundColor: 'rgb(0,128,0)'
-            }
-        ],
+        }]
+            // [{label: 'MCU',
+            // data: mcu_points,
+            // backgroundColor: 'rgb(255,0,0)'
+            // },
+            // {label: 'DCEU',
+            // data: dceu_points,
+            // backgroundColor: 'rgb(0,0,255)'
+            // },
+            // {label: 'X-Men',
+            // data: xmen_points,
+            // backgroundColor: 'rgb(255,255,0)'
+            // },
+            // {label: 'Batman',
+            // data: batman_points,
+            // backgroundColor: 'rgb(0,0,0)'
+            // },
+            // {label: 'Superman',
+            // data: supe_points,
+            // backgroundColor: 'rgb(0,128,0)'
+            // }]
+        ,
     };
 
     // chart config
@@ -147,7 +162,7 @@ d3.json("../Resources/final_MvDC.json").then(json => {
             plugins: {
                 title: {
                     display: true,
-                    text: "Gross Revenue of Major Superhero Film Series"
+                    text: "Gross Revenue of Superhero Films"
                 }
             }
         }
@@ -163,26 +178,98 @@ d3.json("../Resources/final_MvDC.json").then(json => {
 
     // CHART 2
     // structure data
-    var bubbles = []
+    var mcu_bubbles = []
+    var dceu_bubbles = []
+    var xmen_bubbles = []
+    var supe_bubbles = []
+    var batman_bubbles = []
+    
+    var mcu_films = json_data.filter(film => film.universe_code === 1000)
+    console.log(mcu_films)
 
-    json_data.forEach(item => {
+    var dceu_films = json_data.filter(film => film.universe_code === 2000)
+    console.log(dceu_films)
+
+    var xmen_films = json_data.filter(film => film.universe_code === 3000)
+    console.log(xmen_films)
+
+    var supe_films = json_data.filter(film => film.universe_code === 4000)
+    console.log(supe_films)
+
+    var batman_films = json_data.filter(film => film.universe_code === 5000)
+    console.log(batman_films)
+    
+
+
+    mcu_films.forEach(item => {
         var bubble = {
             x: item.tom_aud_score,
             y: item.tomato_meter,
             r: item.imdb_gross/50000000
         }
-        bubbles.push(bubble)
+        mcu_bubbles.push(bubble)        
     })
 
-        console.log(bubbles)
+    dceu_films.forEach(item => {
+        var bubble = {
+            x: item.tom_aud_score,
+            y: item.tomato_meter,
+            r: item.imdb_gross/50000000
+        }
+        dceu_bubbles.push(bubble)        
+    })
+
+    xmen_films.forEach(item => {
+        var bubble = {
+            x: item.tom_aud_score,
+            y: item.tomato_meter,
+            r: item.imdb_gross/50000000
+        }
+        xmen_bubbles.push(bubble)        
+    })
+
+    supe_films.forEach(item => {
+        var bubble = {
+            x: item.tom_aud_score,
+            y: item.tomato_meter,
+            r: item.imdb_gross/50000000
+        }
+        batman_bubbles.push(bubble)        
+    })
+
+    batman_films.forEach(item => {
+        var bubble = {
+            x: item.tom_aud_score,
+            y: item.tomato_meter,
+            r: item.imdb_gross/50000000
+        }
+        supe_bubbles.push(bubble)        
+    })
 
     // chart setup
     const data2 = {
-        datasets: [{
-          label: 'Films',
-          data: bubbles,
-          backgroundColor: 'rgb(255, 99, 132)'
-        }]
+        datasets: [
+            {label: 'MCU',
+            data: mcu_bubbles,
+            backgroundColor: 'rgb(255,0,0)'
+            },
+            {label: 'DCEU',
+            data: dceu_bubbles,
+            backgroundColor: 'rgb(0,0,255)'
+            },
+            {label: 'X-Men',
+            data: xmen_bubbles,
+            backgroundColor: 'rgb(255,255,0)'
+            },
+            {label: 'Batman',
+            data: batman_bubbles,
+            backgroundColor: 'rgb(0,0,0)'
+            },
+            {label: 'Superman',
+            data: supe_bubbles,
+            backgroundColor: 'rgb(0,128,0)'
+            }
+        ]
       };
 
     // chart config
